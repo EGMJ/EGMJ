5fd94c736da120ec93d7e4b8b714c1d4d573ebc1
EGMJ <gabrieledson730@gmail.com>
2024-04-12 17:03:50 -0300

    atualizacoes

---
+++ cursos/Geek University/Exercicios/exer04.py
@@ -0,0 +1,64 @@
+# RESOLVENDO AS EXERCICIOS.
+
+# EXEC 1:
+integer_number = int(input("Digite um numero inteiro: "))
+print(integer_number)
+
+# EXEC 2:
+real_number = float(input("Digite um numero real: "))
+print(real_number)
+
+# EXEC 3:
+integer_value_1 = int(input("Digite um número inteiro 1: "))
+integer_value_2 = int(input("Digite um número inteiro 2: "))
+integer_value_3 = int(input("Digite um número inteiro 3: "))
+
+sum = integer_value_1 + integer_value_2 + integer_value_3
+
+print(f"O valor total é: {sum}")
+
+# EXEC 4:
+real_value = float(input("Digite um número real"))
+square_value = real_value ** 2
+
+print(f"O valor quadratico de {real_value} é {square_value}")
+
+# EXEC 5:
+
+
+
+# EXEC 6:
+# EXEC 7:
+# EXEC 8:
+# EXEC 9:
+# EXEC 10:
+# EXEC 11:
+# EXEC 12:
+# EXEC 13:
+# EXEC 14:
+# EXEC 15:
+# EXEC 16:
+# EXEC 17:
+# EXEC 18:
+# EXEC 19:
+# EXEC 20:
+# EXEC 21:
+# EXEC 22:
+# EXEC 23:
+# EXEC 24:
+# EXEC 25:
+# EXEC 26:
+# EXEC 27:
+# EXEC 28:
+# EXEC 29:
+# EXEC 30:
+# EXEC 3:
+# EXEC 3:
+# EXEC 3:
+# EXEC 3:
+# EXEC 3:
+# EXEC 3:
+# EXEC 3:
+# EXEC 3:
+# EXEC 3:
+# EXEC 4:
